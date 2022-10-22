8fc6428d2471f3ac5d831b641cff9d54c524b795
sroxck <642261686@qq.com>
2022-10-22 13:54:15 +0800

    fix: type error

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,10 @@ import { isRef, vueRef } from './reactivity'
 
 export * from '@vue/reactivity'
 
-export const ref: typeof vueRef = (function (value: any) {
+export const ref = function (value: any) {
   if (isRef(value)) return value
   return createFunctional(vueRef(rawToRef(value)), false)
-}) as any
+}
 
 
 
