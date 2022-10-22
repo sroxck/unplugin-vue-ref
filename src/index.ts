fb61ccbd0e2fce3a57b15918858e8a1c8eb9edea
sroxck <642261686@qq.com>
2022-10-22 13:52:07 +0800

    feat: remove custom functions other than ref to avoid concatenation

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 
 import { createFunctional, rawToRef } from './functional'
-import { isRef, vueRef, vueComputed, vueShallowRef, vueCustomRef, vueReadonly, vueToRef, vueToRefs } from './reactivity'
+import { isRef, vueRef } from './reactivity'
 
 export * from '@vue/reactivity'
 
@@ -9,33 +9,5 @@ export const ref: typeof vueRef = (function (value: any) {
   return createFunctional(vueRef(rawToRef(value)), false)
 }) as any
 
-export const computed: typeof vueComputed = function (getter: any, debugOptions: any) {
-  return createFunctional(vueComputed(getter, debugOptions), typeof getter === 'function')
-}
-
-export const shallowRef: typeof vueShallowRef = (function (value: any) {
-  return createFunctional(vueShallowRef(rawToRef(value)), false)
-}) as any
-
-export const readonly: typeof vueReadonly = function (target: any): any {
-  return createFunctional(vueReadonly(rawToRef(target)), true)
-}
-
-export const customRef: typeof vueCustomRef = function (value: any) {
-  return createFunctional(vueCustomRef(rawToRef(value)), false)
-}
-
-export const toRef: typeof vueToRef = function (obj: any, key: string) {
-  return createFunctional(vueToRef(obj, key), false)
-}
-
-export const toRefs: typeof vueToRefs = function (obj: any) {
-  const refs = vueToRefs(obj)
-  if (typeof refs !== 'object') return refs
-  for (const key of Object.keys(refs)) {
-    refs[key] = createFunctional(refs[key], false)
-  }
-  return refs
-}
 
 
