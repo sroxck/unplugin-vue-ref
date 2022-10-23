fe6a61fc463b3bb494cd1cf65ec4b2bf363529e5
sroxck <642261686@qq.com>
2022-10-23 16:15:44 +0800

    fix: bug

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@
 import { createFunctional, rawToRef } from './functional'
 import { isRef, vueRef } from './reactivity'
 
-export * from '@vue/reactivity'
 
+export * from '@vue/reactivity'
 export const ref = function (value: any) {
   if (isRef(value)) return value
   return createFunctional(vueRef(rawToRef(value)), false)
--- test/main.ts
+++ test/main.ts
@@ -1,3 +1,4 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+       // 20
 createApp(App).mount('#app')
